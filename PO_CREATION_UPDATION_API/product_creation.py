f2d00017ed12047acdf51e010b5093fd6661a380
chintanaarav <chintan.darji@aaravsolutions.com>
2024-10-21 00:36:07 +0530

    Fixed bug in product logic

--- PO_CREATION_UPDATION_API/product_creation.py
+++ PO_CREATION_UPDATION_API/product_creation.py
@@ -1,37 +1,27 @@
-import xmlrpc.client
 from serverconncetion import ServerConnection
 from error_handling import ErrorHandling
 
-class ProductCreation:
-    def __init__(self, product_lines, models, uid, db, password, file_name, file_id, error_folder_id):
-        self.product_lines = product_lines
-        self.file_name = file_name
-        self.file_id = file_id
-        self.error_folder_id = error_folder_id
-        self.models = models  # Use the models passed in the constructor
-        self.uid = uid  # Store UID for use in methods
-        self.db = db  # Store DB for use in methods
-        self.password = password  # Store password for use in methods        
+class ProductCreation:  
 
-    def create_product_with_no(self):
+    def create_product_with_no(self, product_lines, models, uid, db, password, file_name, file_id, error_folder_id):
 
         product_data = {}
 
         try:
             # with material no
-            product_data['id'] = self.product_lines[2]['product_id']  # product id
-            product_data['name'] = self.product_lines[2]['name']  # product name
-            product_data['default_code'] = self.product_lines[2]['product_id']  # internal reference
+            product_data['id'] = product_lines[2]['product_id']  # product id
+            product_data['name'] = product_lines[2]['name']  # product name
+            product_data['default_code'] = product_lines[2]['product_id']  # internal reference
             print(product_data)
 
             # creating product
-            product_ids = self.models.execute_kw(self.db, self.uid, self.password, 'product.product', 'create', [product_data])
+            product_ids = models.execute_kw(db, uid, password, 'product.product', 'create', [product_data])
             print("Created product ->", product_ids)
 
         except (ValueError, TypeError) as e:
             error_message = f"Error during product creation: {str(e)}"
             print(error_message)
-            ErrorHandling.handle_error(self.file_id, self.error_folder_id, self.file_name, error_message, self.uid, self.db, self.password, self.models)
+            ErrorHandling.handle_error(file_id, error_folder_id, file_name, error_message, uid, db, password, models)
 
         return product_ids
 
