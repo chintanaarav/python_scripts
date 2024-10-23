08d7f5bf2a46d3ef2c910adb5188fc8372402dcc
chintanaarav <chintan.darji@aaravsolutions.com>
2024-10-23 17:02:16 +0530

    changes in serverconncetion file

--- PO_CREATION_UPDATION_API/serverconncetion.py
+++ PO_CREATION_UPDATION_API/serverconncetion.py
@@ -1,14 +1,20 @@
 import xmlrpc.client
+from dotenv import load_dotenv
+import os
 
 class ServerConnection:
+     
+     def __init__(self):
+        # Load environment variables from the specified file
+        load_dotenv('/home/odoo/src/myenv/hashkey.env')     
      
      def connection(self):
 
        # Odoo credentials and URL
        url = "https://cocreateaarav-itgnew-itgdev-15781887.dev.odoo.com/"  # Odoo instance URL/IP address
        db = "cocreateaarav-itgnew-itgdev-15781887"  # Odoo database name
-       username = "john@yopmail.com"  # Instance username
-       password = "123456"  # Instance password
+       username = os.getenv("ODOO_USERNAME")  # Fetching username from .env file
+       password = os.getenv("ODOO_PASSWORD")  # Fetching password from .env file
 
 
        #getting server version
